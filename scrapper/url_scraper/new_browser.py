1ceea7fd7d2c1edc9aeb4ec1a48e5f257a49c36f
Abhinav Kumar Aayush <142171230+AbhinavAka@users.noreply.github.com>
2026-03-09 17:01:39 +0530

    alibaba product crawler

--- scrapper/url_scraper/new_browser.py
+++
@@ -1,6 +0,0 @@
-def start_browser(playwright):
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
-
-    return browser, context, page
